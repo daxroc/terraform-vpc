658443ba6aa3d152193be3366a64cfc4dd058537
Damien Roche <dcroche@gmail.com>
2017-09-10 13:36:36 +0100

    Updated outputs

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,6 @@
 output "vpc_id"              { value = "${aws_vpc.default.id}" }
 output "region"              { value = "${var.region}" }
 output "availability_zones"  { value = ["${var.azs}"] }
-output "private_subnet_list" { value = ["${aws_subnet.private.*.id}"] }
 output "public_subnet_list"  { value = ["${aws_subnet.public.*.id}"] }
+output "private_subnet_list" { value = ["${aws_subnet.private.*.id}"] }
+output "cidr_block"          { value = "${aws_vpc.default.cidr_block}" }
